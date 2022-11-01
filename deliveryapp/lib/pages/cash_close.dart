e1265a40d101a5332967ddd7b60c6c30200715c2
Danni-PC-DEBIAN <danielgz.m@hotmail.com>
2022-10-31 20:06:44 -0500

    add cashclose

---
+++ deliveryapp/lib/pages/cash_close.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/drawer.dart';
+
+class CashClosePage extends StatelessWidget {
+  const CashClosePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cierre de caja"),
+      ),
+      drawer: const DrawerWidget(),
+    );
+  }
+}
