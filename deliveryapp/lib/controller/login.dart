5a175da607d6105a9c21aa8b93ebce61767b2e60
Danni-PC-DEBIAN <danielgz.m@hotmail.com>
2022-10-31 20:04:17 -0500

    add arreglos login

--- deliveryapp/lib/controller/login.dart
+++ deliveryapp/lib/controller/login.dart
@@ -1,6 +1,20 @@
+import '../model/entity/repository/Users.dart'; /* siempre importar relativo ---/ */
+
 class Logincontroller {
-  void validatemailpassword() {
+  late final UserRepository _userRepository;
+
+  Logincontroller() {
+    _userRepository = UserRepository();
+  }
+
+  void validatEmailPassword(String email, String password) {
     /* Consultar usuario que tenga el correo dado */
+
+    var user = _userRepository.findByEmail(email);
+
     /* Verificar si la clave es igual a la que esta en BD */
+    if (user.password != password) {
+      throw Exception("Invalid credentials");
+    }
   }
 }
--- deliveryapp/lib/pages/Login.dart
+++ deliveryapp/lib/pages/Login.dart
@@ -1,155 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:deliveryapp/pages/Payments.dart';
 
-class Loginview extends StatelessWidget {
-  const Loginview({super.key});
+class LoginPage extends StatelessWidget {
+  final _imageUrl = "img/images/logoto.png";
+  final late model;
+
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Image.asset("img/images/logoto.png"),
-            const SizedBox(
-              height: 20,
-            ),
-            Flexible(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const TextField(
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '@ Email',
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    const TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '*   Password',
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    ElevatedButton(
-                      child: const Text("Login"),
-                      onPressed: () {},
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    const Text("Login with: "),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                          child: const Text("Google"),
-                          onPressed: () {},
-                        ),
-                        ElevatedButton(
-                          child: const Text("Facebook"),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    TextButton(
-                      child: const Text("Doesn't have an account yet?"),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _logo(),
+              _formulario(context),
+              _inicioAlternativo(),
+              TextButton(
+                child: const Text("No tiene usuario, Registrate aqui"),
+                onPressed: () {},
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-
-
-      /* body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 50,
-              ),
-              Image.asset("img/images/logoto.png"),
-              const SizedBox(
-                height: 25,
-              ),
-              const TextField(
-                obscureText: false,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '@ Email',
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '*   Password',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(onPressed: () {}, child: const Text("Login")),
-              const SizedBox(
-                width: 10,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text("Login with:"),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(onPressed: () {}, child: const Text("Google")),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {}, child: const Text("Facebook")),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text("Doesn't have an account yet?")),
-            ],
-          ),
+  Widget _logo() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
         ),
+        Image.asset(_imageUrl),
+        const SizedBox(
+          height: 30,
         ),
-      );
-    }
+      ],
+    );
   }
-  */
+
+  Widget _formulario(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          _campoCorreoElectronico(),
+          const SizedBox(height: 8),
+          _campoClave(),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            child: const Text(
+              "Iniciar sesion",
+              style: TextStyle(fontSize: 24),
+            ),
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                // TODO: Validar usuario y contraseña en BD
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const PaymentsPage(),
+                  ),
+                );
+              }
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _campoCorreoElectronico() {
+    return TextFormField(
+      maxLength: 50,
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Correo Electrónico',
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "El correo electronico es obligatorio";
+        }
+        if (!value.contains("@") || !value.contains(".")) {
+          return "El correo tiene un formato inválido";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _campoClave() {
+    return TextFormField(
+      maxLength: 30,
+      obscureText: true,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Contraseña',
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "La contraseña es obligatoria";
+        }
+        if (value.length < 6) {
+          return "Minimo debe contener 6 caracteres";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _inicioAlternativo() {
+    return Column(
+      children: [
+        const Text("O iniciar sesion con"),
+        const SizedBox(
+          height: 8,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              child: const Text("Facebook"),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: const Text("Google"),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+      ],
+    );
+  }
+}
