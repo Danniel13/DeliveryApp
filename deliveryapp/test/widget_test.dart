5d4293d188401f3500f9061b0bdd6ad97bb3984c
Danni-PC-DEBIAN <danielgz.m@hotmail.com>
2022-11-01 21:06:24 -0500

    add myapp

--- deliveryapp/test/widget_test.dart
+++ deliveryapp/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:deliveryapp/main.dart';
+import 'package:deliveryapp/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
