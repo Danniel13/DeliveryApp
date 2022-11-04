ee7f671e1833826c914805ebce645e943c7ed636
Danni-PC-DEBIAN <danielgz.m@hotmail.com>
2022-11-03 23:44:35 -0500

    add asynck main

--- deliveryapp/lib/main.dart
+++ deliveryapp/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 /* importar ubicaciones relativas-corresponden a las exactas del directorio */
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
